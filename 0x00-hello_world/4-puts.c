d9a5b8011491d544706aab72292d3178163cf218
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-15 05:38:22 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-int main(void){
-
-
+/**
+ * Description: main prints out a string
+ * Return: 0 if succesful
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
-
-
 return (0);
 }
