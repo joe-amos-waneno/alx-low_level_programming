f48608e7ff0b4847b4d2a814edb563940f092657
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-16 07:32:20 -0700

    Single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
