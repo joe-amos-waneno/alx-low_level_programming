0fd675fd5ce332a149dadf0e8f84b873f5d2132a
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-16 06:15:48 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        char alphabet;
+	
+	char capital;
+
+	capital = 'A';
+        alphabet = 'a';
+        while (alphabet <= 'z')
+        {
+                putchar(alphabet);
+                alphabet++;
+        }
+	while (capital <= 'Z')
+        {
+                putchar(capital);
+                capital++;
+        }
+        putchar('\n');
+        return (0);
+
+}
