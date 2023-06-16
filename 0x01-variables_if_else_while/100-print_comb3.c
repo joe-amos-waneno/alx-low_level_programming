42b7cd139682c48e8a27eea065c1ef8b29ae5e96
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-16 08:22:20 -0700

    Combination of numbers without repetition

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,30 +10,19 @@ int main(void)
 
 	int num2;
 
-	num1 = '0';
-	num2 = '0';
-	while (num1 <= '9')
+	for (num1 = 0; num1 < 10; num1++)
 	{
-		while (num2 <= '9')
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			if (!(num1 > num2) || num1 == num2)
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			if (num1 != 8 || num2 != 9)
 			{
-				putchar(num1);
-				putchar(num2);
-				if (num1 == '8' && num2 == '9')
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
-			num2++;
 		}
-		num2 = '0';
-		num1++;
 	}
+	putchar('\n');
 	return (0);
 }
