aafb69c2e9a5f123c893ac371f8a2fbed5ebfeb7
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-20 02:37:49 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+	}
+	_putchar('\n');
+}
