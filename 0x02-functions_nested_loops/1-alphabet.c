cda9c89fbd9ac59a0836003f697983602cd978b9
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-20 02:24:09 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * alphabet - Entry point
+ *
+ * Return: void
+ */
+void alphabet(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
