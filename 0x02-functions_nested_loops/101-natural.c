69d26bdad2199a0196aedf9b0ff542a1ddac0cfe
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-20 06:15:25 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a, b = 0;
+
+	while (a < 1024)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+		{
+			b += a;
+		}
+		a++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
