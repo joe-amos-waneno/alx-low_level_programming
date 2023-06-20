dfc3a30ec801b1947c28f10980b102f34153bfb6
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-20 02:43:19 -0700

    print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,12 @@
 void print_alphabet_x10(void)
 {
 	char alpha;
-	
 	int num;
 
 	for (num = 0; num < 10; num++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
