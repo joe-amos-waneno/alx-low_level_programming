bfbef914f9f04f3df0b1d67c469b9ea82bf98c6d
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-20 06:31:19 -0700

    Fibbonacci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: Always
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+		{
+			printf("%d", j);
+		}
+		else if (i == 1)
+		{
+			printf(", %d", k);
+		}
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %d", k);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
