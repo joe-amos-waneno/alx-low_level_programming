e595d22121cdebc264029f7417c1be0523d7061d
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-22 07:00:16 -0700

    Digits

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * print_numbers - Entry point
+ * print_numbers - checks for checks for a digit (0 through 9).
  *
- * Return: ALways 0
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar(num);
+		_putchar(num + '0');
 	}
 	_putchar('\n');
 }
