7bfaf64ac395fab7b26c9c3001bffa15869f6679
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-22 06:05:56 -0700

    digits

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - Entry point
+ * print_numbers -Entry point
  *
  * Return: Always 0.
  */
 void print_numbers(void)
 {
-	int c;
+ int c;
 
-	for (c = 48; c < 58; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+ for (c = 48; c < 58; c++)
+ {
+ _putchar(c);
+ }
+ _putchar('\n');
 }
+
