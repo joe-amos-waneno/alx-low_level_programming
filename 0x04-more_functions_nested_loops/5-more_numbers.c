a7e136c3440148b2e551fa16c0f4ef0139dce474
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-22 07:30:29 -0700

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,10 @@ void more_numbers(void)
 	{
 		for (num2 = 0; num2 < 15; num2++)
 		{
-			_putchar(num2 / 10 + '0');
+			if (num2 >= 10)
+			{
+				_putchar(num2 / 10 + '0');
+			}
 			_putchar(num2 % 10 + '0');
 		}
 		_putchar('\n');
