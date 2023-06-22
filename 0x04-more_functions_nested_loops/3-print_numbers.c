30578d3c6c7bde7650e8296699df304c03d9d9d4
joe-amos-waneno <waneno.amos@gmail.com>
2023-06-22 06:11:54 -0700

    Digits

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,11 @@
  */
 void print_numbers(void)
 {
- int c;
+	int num;
 
- for (c = 48; c < 58; c++)
- {
- _putchar(c);
- }
- _putchar('\n');
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
 }
-
