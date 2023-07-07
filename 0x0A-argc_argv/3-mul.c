bf2eda584b5f41c3e2123bf69b954cc642b3582b
joe-amos-waneno <waneno.amos@gmail.com>
2023-07-07 04:24:47 -0700

    Multiple of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - ENtry point
+ * @s: string to be converted
+ * Return: int converted from string
+ */
+int _atoi(char *s)
+{
+	int x, y, z, len, f, digit;
+
+	x = 0;
+	y = 0;
+	z = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (x < len && f == 0)
+	{
+		if (s[x] == '-')
+			++y;
+
+		if (s[x] >= '0' && s[x] <= '9')
+		{
+			digit = s[x] - '0';
+			if (y % 2)
+				digit = -digit;
+			z = z * 10 + digit;
+			f = 1;
+			if (s[x + 1] < '0' || s[x + 1] > '9')
+				break;
+			f = 0;
+		}
+		x++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (z);
+}
+
+/**
+ * main - main entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
