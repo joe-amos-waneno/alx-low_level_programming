f6250809a7c10a01f91adbc04cc6f8fe32f55a07
joe-amos-waneno <waneno.amos@gmail.com>
2023-07-11 05:39:03 -0700

    Adding input

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int x, y;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	x = y = 0;
+	while (s1[x] != '\0')
+		x++;
+	while (s2[y] != '\0')
+		y++;
+	concat = malloc(sizeof(char) * (x + y + 1));
+
+	if (concat == NULL)
+		return (NULL);
+	x = y = 0;
+	while (s1[x] != '\0')
+	{
+		concat[x] = s1[x];
+		x++;
+	}
+
+	while (s2[y] != '\0')
+	{
+		concat[x] = s2[y];
+		x++, y++;
+	}
+	concat[x] = '\0';
+	return (concat);
+}
