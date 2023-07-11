74f6613de5aa42a358212ef5a4c90ac9dd968e7b
joe-amos-waneno <waneno.amos@gmail.com>
2023-07-11 05:32:07 -0700

    Duplicating memory space location

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - Entry point
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int x, y = 0;
+
+	if (str == NULL)
+		return (NULL);
+	x = 0;
+	while (str[x] != '\0')
+		x++;
+
+	dup = malloc(sizeof(char) * (x + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (y = 0; str[y]; y++)
+		dup[y] = str[y];
+
+	return (dup);
+}
