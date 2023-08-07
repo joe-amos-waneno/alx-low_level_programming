f450d75061217de478f6eaa750be151b0d7cb223
joe-amos-waneno <waneno.amos@gmail.com>
2023-08-07 15:06:12 -0700

    Creating a file in file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	y = write(fd, text_content, len);
+	y = write(x, text_content, z);
 
 	if (x == -1 || y == -1)
 		return (-1);
