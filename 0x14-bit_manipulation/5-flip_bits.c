54536c8ba04758967e5da76c6ded0f06e39ce5ab
joe-amos-waneno <waneno.amos@gmail.com>
2023-08-31 07:48:05 -0700

    flipping bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * flip_bits - Entry point
- * @n: first num
- * @m: second num
+ * @m: second number
+ * @n: first number
  * Return: 0 always
- */ 
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, z = 0;
