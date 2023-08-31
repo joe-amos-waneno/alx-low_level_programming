1b62cad6d4bf1583851f98cdd9f1f16c3e5ab8e2
joe-amos-waneno <waneno.amos@gmail.com>
2023-08-31 07:09:54 -0700

    Binary to units

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * binary_to_uint - Entry point
+ * @b: string
+ * Return: 0 always
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int x;
+	unsigned int y = 0;
+
+	if (!b)
+		return (0);
+
+	for (x = 0; b[x]; x++)
+	{
+		if (b[x] < '0' || b[x] > '1')
+			return (0);
+		y = 2 * y + (b[x] - '0');
+	}
+
+	return (y);
+}
