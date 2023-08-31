15160b1249c58bc02dddaa1c7938c7841c438284
joe-amos-waneno <waneno.amos@gmail.com>
2023-08-31 07:40:22 -0700

    clearing bits

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,12 +5,10 @@
  * @index: index
  * Return: 0 always
  */
- 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
-
 	*n = (~(1UL << index) & *n);
 	return (1);
 }
