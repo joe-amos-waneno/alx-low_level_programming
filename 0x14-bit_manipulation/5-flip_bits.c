5a76b38adcfa56736b4c8ecb489cc495ad348012
joe-amos-waneno <waneno.amos@gmail.com>
2023-08-31 07:31:33 -0700

    Number of bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * flip_bits - Entry point
+ * @n: first num
+ * @m: second num
+ * Return: 0 always
+ */
+ 
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int x, q = 0;
+	unsigned long int cn;
+	unsigned long int a = n ^ m;
+
+	for (x = 63; x >= 0; x--)
+	{
+		cn = a >> x;
+		if (cn & 1)
+			q++;
+	}
+
+	return (q);
+}
