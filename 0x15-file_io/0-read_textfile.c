501522aa8bd9cb7ddeea58b985daf95e073e930e
joe-amos-waneno <waneno.amos@gmail.com>
2023-09-04 07:02:10 -0700

    reading text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,29 +2,25 @@
 #include <stdlib.h>
 /**
  * read_textfile - Entry point
- * @filename: text
+ * @filename: text file
  * @letters: number of letters
- * Return: 0 always
+ * Return: 0 Awalys
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
+	char *bufferr;
 	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
-	
-	fd = open(filename, O_RDONLY);
 
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	
-	t = read(fd, buffer, letters);
-	
-	w = write(STDOUT_FILENO, buffer, t);
-	
-	free(buffer);
-	
+	bufferr = malloc(sizeof(char) * letters);
+	t = read(fd, bufferr, letters);
+	w = write(STDOUT_FILENO, bufferr, t);
+
+	free(bufferr);
 	close(fd);
 	return (w);
 }
