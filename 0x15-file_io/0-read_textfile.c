0c306a09720257fd8990ca8932fae4a2a7cfb41a
joe-amos-waneno <waneno.amos@gmail.com>
2023-09-04 06:56:57 -0700

    reading text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,21 +12,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
-
-	fd = open(filename, O_RDONLY);
 	
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
-	
 	buffer = malloc(sizeof(char) * letters);
 	
 	t = read(fd, buffer, letters);
 	
 	w = write(STDOUT_FILENO, buffer, t);
-
+	
 	free(buffer);
 	
 	close(fd);
-	
 	return (w);
 }
