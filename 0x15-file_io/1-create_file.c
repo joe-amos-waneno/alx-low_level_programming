5e5fc2e295df55f91c2f01fbce9aeaa50efa17bd
joe-amos-waneno <waneno.amos@gmail.com>
2023-09-04 07:10:26 -0700

    Creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int df, trx, len = 0;
+	int rs, vw, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -17,10 +17,14 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	df = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	trx = write(df, text_content, len);
-	if (df == -1 || trx == -1)
+
+	rs = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	vw = write(rs, text_content, len);
+
+	if (rs == -1 || vw == -1)
 		return (-1);
-	close(df);0
+
+	close(rs);
+
 	return (1);
 }
