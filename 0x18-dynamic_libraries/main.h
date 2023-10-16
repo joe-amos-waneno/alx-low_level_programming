ec02b9c863ea92cbbc1b88d38a2a2975b6c6effd
joe-amos-waneno <waneno.amos@gmail.com>
2023-10-16 12:52:20 -0700

    main.h and dynamic library function

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isalpha(int c);
+int _abs(int n);
+int _islower(int c);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+int _putchar(char c);
+char *_strstr(char *haystack, char *needle);
+int _isupper(int c);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+void _puts(char *s);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
+
+#endif
