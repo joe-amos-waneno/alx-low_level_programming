0c8604dbc488c06893c50e4162096d5f2cffa8ee
joe-amos-waneno <waneno.amos@gmail.com>
2023-11-03 06:04:14 -0700

    corr

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -7,6 +7,24 @@
  */
 shash_table_t *shash_table_create(unsigned long int size)
 {
+	shash_table_t *new_table;
+	unsigned long int index;
+
+	new_table = (shash_table_t *) malloc(sizeof(shash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+	new_table->size = size;
+	new_table->shead = NULL;
+	new_table->stail = NULL;
+	new_table->array = (shash_node_t **) malloc(sizeof(shash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	for (index = 0; index < size; index++)
+		(new_table->array)[index] = NULL;
+	return (new_table);
 
 }
 
@@ -19,9 +37,7 @@ shash_table_t *shash_table_create(unsigned long int size)
  */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
-
 }
-
 /**
  * shash_table_get - ENTRY
  * @ht: hash table
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,5 +6,26 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *current_node, *previous_node;
+	unsigned long int array_index, has_nodes;
+
+	for (array_index = 0; array_index < ht->size; array_index++)
+	{
+		has_nodes = 0;
+		current_node = (ht->array)[array_index];
+		while (current_node)
+		{
+			has_nodes = 1;
+			previous_node = current_node;
+			current_node = current_node->next;
+			free(previous_node->key);
+			free(previous_node->value);
+			free(previous_node);
+		}
+		if (!has_nodes)
+			free((ht->array)[array_index]);
+	}
+	free(ht->array);
+	free(ht);
 
 }
